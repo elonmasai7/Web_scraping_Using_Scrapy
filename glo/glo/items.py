10d60230434917178b83eead5bc3a4603245e7fd
koleshjr <stevekolesh975@gmail.com>
2023-09-29 17:15:30 +0300

    full pipeline yaaay

--- glo/glo/items.py
+++ glo/glo/items.py
@@ -16,19 +16,7 @@ def try_float(value):
     except ValueError:
         return value
     
-def extract_location(value):
-    #2,3 and 4 Bedroom Apartment For Sale In Kileleshwa, get Kileleshwa
-    return value.split(' ')[-1]
-
-def extract_service_type(value):
-    #2,3 and 4 Bedroom Apartment For Sale In Kileleshwa, get For Sale
-    return value.split(' ')[-3]
-
-
-
-
-
-class GloItem(scrapy.Item):
+class GloParentItem(scrapy.Item):
     house_href = scrapy.Field(
         input_processor=MapCompose(remove_tags, str.strip),
         output_processor=TakeFirst()
@@ -38,18 +26,25 @@ class GloItem(scrapy.Item):
         input_processor=MapCompose(remove_tags,remove_commas, try_float),
         output_processor=TakeFirst()
     )
+
     bed_rooms = scrapy.Field(
         input_processor=MapCompose(remove_tags, str.strip, int),
         output_processor=TakeFirst()
     )
-    
+
+
+class GloChildItem(scrapy.Item):
     house_location = scrapy.Field(
-        input_processor=MapCompose(remove_tags, str.strip, extract_location),
+        input_processor=MapCompose(remove_tags, str.strip),
         output_processor=TakeFirst()
     )
 
     service_type = scrapy.Field(
-        input_processor=MapCompose(remove_tags, str.strip, extract_service_type),
+        input_processor=MapCompose(remove_tags, str.strip),
         output_processor=TakeFirst()
     )
 
+    property_type = scrapy.Field(
+        input_processor=MapCompose(remove_tags, str.strip),
+        output_processor=TakeFirst()
+    )
--- glo/glo/spiders/glorealtors.py
+++ glo/glo/spiders/glorealtors.py
@@ -1,5 +1,5 @@
 import scrapy
-from glo.items import GloItem
+from glo.items import GloParentItem, GloChildItem
 from scrapy.loader import ItemLoader
 
 class GlorealtorsSpider(scrapy.Spider):
@@ -10,12 +10,32 @@ class GlorealtorsSpider(scrapy.Spider):
 
     def parse(self, response):
         for house in response.css('div.item-listing-wrap'):
-            item = ItemLoader(item=GloItem(), selector=house)
-            item.add_css("house_href", "div.item-body h2 > a::attr(href)")
-            yield item.load_item()
+            parent_item = ItemLoader(item=GloParentItem(), selector=house)
+            
+            parent_item.add_css("house_href", "div.item-body h2 > a::attr(href)")
+            parent_item.add_css("house_price", "div.item-body ul li.item-price::text")
+            parent_item.add_css("bed_rooms", "ul.item-amenities li.h-beds > span.hz-figure::text")
+            
+            # Extract the child page URL
+            child_page_url = house.css('div.item-body h2 > a::attr(href)').extract_first()
+            if child_page_url:
+                yield scrapy.Request(url=child_page_url, callback=self.parse_child_page, meta={'parent_item': parent_item.load_item()})
 
         # Check for the "Next" button
         next_page = response.css('ul.pagination li.page-item.active + li.page-item a.page-link::attr(href)').extract_first()
         if next_page:
             self.page_number += 1
             yield scrapy.Request(url=next_page, callback=self.parse)
+
+    def parse_child_page(self, response):
+        parent_item = response.meta['parent_item']
+        child_item = ItemLoader(item=GloChildItem(), response=response)
+        
+        # Use your actual CSS selectors for "house_location," "service_type," and "property_type"
+        child_item.add_css("house_location", "li.detail-area > span::text")
+        child_item.add_css("service_type", "li.prop_status > span::text")
+        child_item.add_css("property_type", "li.prop_type > span::text")
+        
+        # Merge parent and child items into one
+        merged_item = {**parent_item, **child_item.load_item()}
+        yield merged_item
