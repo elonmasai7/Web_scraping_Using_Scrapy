c7683eff17849099ebbcadd449b5cb74fa2e1bee
Elon masai <123534505+elonmasai7@users.noreply.github.com>
2024-11-15 07:43:22 +0000

    new features

--- buyrentkenya/buyrentkenya/settings.py
+++ buyrentkenya/buyrentkenya/settings.py
@@ -1,93 +1,87 @@
 # Scrapy settings for buyrentkenya project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 BOT_NAME = "buyrentkenya"
 
 SPIDER_MODULES = ["buyrentkenya.spiders"]
 NEWSPIDER_MODULE = "buyrentkenya.spiders"
 
+# ----------------------------------------
+# Crawler Identification and Behavior
+# ----------------------------------------
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "buyrentkenya (+http://www.yourdomain.com)"
-
-# Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# ----------------------------------------
+# Request Configuration
+# ----------------------------------------
 
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS = 32
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
-# Disable cookies (enabled by default)
+# Disable cookies if not needed
 #COOKIES_ENABLED = False
 
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# ----------------------------------------
+# AutoThrottle Configuration
+# ----------------------------------------
 
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-#}
+AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_START_DELAY = 1  # Initial download delay
+AUTOTHROTTLE_MAX_DELAY = 10   # Maximum delay in case of high latencies
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0  # Average number of parallel requests
+AUTOTHROTTLE_DEBUG = False  # Set to True to show throttling stats
 
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "buyrentkenya.middlewares.BuyrentkenyaSpiderMiddleware": 543,
-#}
+# ----------------------------------------
+# Middleware Settings
+# ----------------------------------------
 
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "buyrentkenya.middlewares.BuyrentkenyaDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'myproject.middlewares.RotateUserAgentMiddleware': 400,
+    'myproject.middlewares.ProxyMiddleware': 410,
+    # "buyrentkenya.middlewares.BuyrentkenyaDownloaderMiddleware": 543,  # Uncomment if needed
+}
 
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
+# ----------------------------------------
+# scrapy-redis Configuration for Distributed Crawling
+# ----------------------------------------
 
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "buyrentkenya.pipelines.BuyrentkenyaPipeline": 300,
-#}
+DUPEFILTER_CLASS = 'scrapy_redis.dupefilter.RFPDupeFilter'
+SCHEDULER = 'scrapy_redis.scheduler.Scheduler'
+SCHEDULER_PERSIST = True
 
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
 
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+# ----------------------------------------
+# Item Pipelines
+# ----------------------------------------
+
+ITEM_PIPELINES = {
+    'myproject.pipelines.MongoDBPipeline': 300,
+}
+
+# MongoDB Connection Details
+MONGO_URI = 'mongodb://localhost:27017'
+MONGO_DATABASE = 'scrapy_db'
+
+# ----------------------------------------
+# HTTP Cache (optional)
+# ----------------------------------------
+
 #HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = "httpcache"
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
 
-# Set settings whose default value is deprecated to a future-proof value
+# ----------------------------------------
+# Additional Settings
+# ----------------------------------------
+
+# Set future-proof default values
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
